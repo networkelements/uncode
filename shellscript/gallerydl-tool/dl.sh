3eecf7699a1524140b13075c6ad7fb5f0dfdd59e
networkelements <2743389+networkelements@users.noreply.github.com>
2020-10-25 18:20:10 +0900

    Update dl.sh

--- shellscript/gallerydl-tool/dl.sh
+++ shellscript/gallerydl-tool/dl.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+url=`cat $HOMEdlurlall.txt`
+history="$HOME/gallery-dl-history.bin"
+dl_directory="$HOME/gallery-dl/"
+send_directory="s3://mybucket/gallery-dl/"
 disk_area01=/
 area_limit01=90
 disk_limit01=`df $disk_area01 | tail -1 | /bin/sed 's/^.* \([0-9]*\)%.*$/\1/'`
+count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+process_name=s3cmd
 
-url=`cat dlurlall.txt`
-history="$HOME/gallery-dl-history.bin"
 
 while read line
 do
@@ -17,6 +21,15 @@ done << FILE
 $url    
 FILE
 
-s3cmd put --recursive $HOME/gallery-dl/ s3://mybucket/gallery-dl/
+s3cmd put --recursive $dl_directory $send_directory
+
+if [ $count = 0 ]; then
+
+  echo "$process_name is Down"
+  echo "Delete gallery-dl directory is Start"
+  rm -rf $dl_directory
+else
+  echo "Disk space is OK"
+fi
 
 exit
