064ddfc620eb7b0df02a24056bd352e0d7f84a34
networkelements <2743389+networkelements@users.noreply.github.com>
2020-11-14 01:17:15 +0900

    Update dl.sh

--- shellscript/gallerydl-tool/dl.sh
+++ shellscript/gallerydl-tool/dl.sh
@@ -4,6 +4,8 @@ url=`cat $HOME/dlurlall.txt`
 history="$HOME/gallery-dl-history.bin"
 dl_directory="$HOME/gallery-dl/"
 send_directory="s3://pic/gallery-dl/"
+ls_s3directory_report="$HOME/s3ls.log"
+ls_localdirectory_report="$HOME/local_ls.log"
 disk_area01=/
 area_limit01=90
 process_name=s3cmd
@@ -20,32 +22,41 @@ done << FILE
 $url
 FILE
 
-echo "Downloaded !!!"
+echo ""
+echo "* Downloaded !!!"
 echo "------------------------------------"
 echo ""
 
 
-s3cmd put --recursive $dl_directory $send_directory ; count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+s3cmd put --recursive $dl_directory $send_directory                          ; \
+ s3cmd ls --recursive $dl_directory > $ls_s3directory_report                 ; \
+ sed -i -e 's/^.*s3.*gallery-dl/gallery-dl/g' $ls_s3directory_report         ; \
+ find $HOME/gallery-dl/ -type f | xargs -r ls -l > $ls_localdirectory_report ; \
+ count=`ps -ef | grep $process_name | grep -v grep | wc -l`
 
 
-echo "Uploaded to Objectstorage"
+echo ""
+echo "* Uploaded to Objectstorage"
 echo "------------------------------------"
 echo ""
 
 
 if [ $count = 0 ]; then
-    echo "$process_name is Down"
+    echo ""
+    echo "* $process_name is Down"
     echo "------------------------------------"
     echo ""
-    echo "Delete gallery-dl directory is Start"
+    echo "* Delete gallery-dl directory is Start"
     echo "------------------------------------"
     echo ""
     rm -rf $dl_directory
-    echo "gallery-dl directory deleted"
+    echo ""
+    echo "* gallery-dl directory deleted"
     echo "------------------------------------"
     echo ""
 else
-    echo "Disk space is OK"
+    echo ""
+    echo "* Disk space is OK"
     echo "------------------------------------"
     echo ""
 fi
