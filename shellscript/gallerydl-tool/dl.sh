1dfab494dca8f3d7cc0e1c422aa9b8972c0ce7d8
networkelements <2743389+networkelements@users.noreply.github.com>
2020-11-28 07:34:21 +0900

    Update dl.sh

--- shellscript/gallerydl-tool/dl.sh
+++ shellscript/gallerydl-tool/dl.sh
@@ -14,6 +14,32 @@ process_name=s3cmd
 while read line
 do
     gallery-dl $line --download-archive $history
+    s3cmd put --recursive $dl_directory $send_directory ; \
+    count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+    
+    if [ $count = 0 ]; then
+    echo ""
+    echo "* $process_name is Down"
+    echo "------------------------------------"
+    echo ""
+    echo "* Delete gallery-dl directory is Start"
+    echo "------------------------------------"
+    echo ""
+    rm -rf $dl_directory
+    echo ""
+    echo "* gallery-dl directory deleted"
+    echo "------------------------------------"
+    echo ""
+else
+    echo ""
+    echo "* Disk space is OK"
+    echo "------------------------------------"
+    echo ""
+fi
+    
+    
+    
+    
     disk_limit01=`df $disk_area01 | tail -1 | /bin/sed 's/^.* \([0-9]*\)%.*$/\1/'`
     if [ $disk_limit01 -gt $area_limit01 ]; then
         break
@@ -28,11 +54,14 @@ echo "------------------------------------"
 echo ""
 
 
-s3cmd put --recursive $dl_directory $send_directory                          ; \
- s3cmd ls --recursive $send_directory > $ls_s3directory_report               ; \
- sed -i -e 's/^.*s3.*gallery-dl/gallery-dl/g' $ls_s3directory_report         ; \
- find $HOME/gallery-dl/ -type f | xargs -r ls -l > $ls_localdirectory_report ; \
- count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+# s3cmd put --recursive $dl_directory $send_directory                          ; \
+# s3cmd ls --recursive $send_directory > $ls_s3directory_report               ; \
+# sed -i -e 's/^.*s3.*gallery-dl/gallery-dl/g' $ls_s3directory_report         ; \
+# find $HOME/gallery-dl/ -type f | xargs -r ls -l > $ls_localdirectory_report ; \
+# count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+
+count=`ps -ef | grep $process_name | grep -v grep | wc -l`
+
 
 echo ""
 echo "* Uploaded to Objectstorage"
