495a12f5f3a1febb8db186c46cb8e31ac92cf3b5
networkelements <you@example.com>
2021-09-20 08:23:14 +0900

    add

---
+++ shellscript/gallerydl-tool/ytdl.sh
@@ -0,0 +1,26 @@
+# * Arknights JP
+
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCvoQlzEzqa6vQA8hq9GNNug/videos"
+
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCvoQlzEzqa6vQA8hq9GNNug/featured"
+
+
+# * Arknights worldwide 
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UC4lLoQ7OQu8m1K_FDa8wefA"
+
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/c/ArknightsWorldwide/videos"
+
+# * Arknights bilibili
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://space.bilibili.com/161775300"
+
+# * dollsfrontline JP
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCKIdwu806-KocBJufLAeArA"
+
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCKIdwu806-KocBJufLAeArA/videos"
+
+# * girlsfrontline EN
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCiiOTrZSdB1C9nxOle-86eg"
+
+youtube-dl --download-archive $HOME/youtube-dl-history.bin "https://www.youtube.com/channel/UCiiOTrZSdB1C9nxOle-86eg/videos"
+
+
